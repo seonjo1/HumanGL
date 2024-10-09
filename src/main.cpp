efcc90f335a3b9b8bd6bca5a76bc480d81498951
seonjo1 <tjdgus9684@gmail.com>
2024-10-09 19:14:13 +0900

    Update main.cpp: OpenGL 버전 4.5로 변경

--- src/main.cpp
+++ src/main.cpp
@@ -29,8 +29,8 @@ int main(int argc, const char** argv) {
 		return -1;
 	}
 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);;
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);;
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_SAMPLES, 4);
 	
