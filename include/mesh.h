3b8addf2bcb3306b7aacf9a8485b381a8282b5bb
seonjo1 <tjdgus9684@gmail.com>
2024-10-09 22:04:04 +0900

    Update mesh: 안 쓰는 함수, Vertex의 color 삭제

--- include/mesh.h
+++ include/mesh.h
@@ -4,7 +4,6 @@
 # include "humanGL.h"
 # include "vertexArray.h"
 # include "buffer.h"
-# include <random>
 
 struct Vertex {
 	glmath::vec3 pos;
@@ -12,42 +11,19 @@ struct Vertex {
 	glmath::vec3 normal;
 };
 
-// class Material {
-// public:
-// 	Material() = default;
-// 	void setMaterial(glload::Material& materialInfo);
-
-// private:
-// 	float m_Ka[3] { 0.2f, 0.2f, 0.2f };
-// 	float m_Kd[3] { 0.8f, 0.8f, 0.8f };
-// 	float m_Ks[3] { 0.5f, 0.5f, 0.5f };
-// 	float m_Ns { 50.0f };
-// 	float m_Ni { 1.0f };
-// 	float m_d { 1.0f };
-// 	uint32_t m_illum { 2 };
-// };
-
 class Mesh {
 public:
-	static std::unique_ptr<Mesh> createPlane();
 	static std::unique_ptr<Mesh> createBox();
-	static std::unique_ptr<Mesh> createMesh(std::vector<Vertex>& vertices,
-											std::vector<uint32_t>& indices,
-											std::vector<glmath::vec3>& colors, 
-											glload::ObjInfo* objInfo);
 	void draw();
 
 private:
 	Mesh() = default;
-	void init(std::vector<Vertex>& vertices, std::vector<uint32_t>& indices, std::vector<glmath::vec3>& colors);
-	// void initMaterial(glload::Material& materialInfo);
+	void init(std::vector<Vertex>& vertices, std::vector<uint32_t>& indices);
 
 	std::unique_ptr<VertexArray> m_vertexArray;
 	std::shared_ptr<Buffer> m_vertexBuffer; 
-	std::shared_ptr<Buffer> m_vertexColorBuffer; 
 	std::shared_ptr<Buffer> m_elementBuffer;
 	GLsizei m_elementSize;
-	// Material m_material;
 };
 
 #endif
--- src/mesh.cpp
+++ src/mesh.cpp
@@ -1,55 +1,36 @@
 #include "../include/mesh.h"
 
-std::unique_ptr<Mesh> Mesh::createPlane() {
-	std::vector<Vertex> vertices = { Vertex{glmath::vec3(0.5f, 0.5f, 0.0f), glmath::vec2(1.0f, 1.0f), glmath::vec3(0.0f, 0.0f, 0.0f)},
-								 	 Vertex{glmath::vec3(0.5f, -0.5f, 0.0f), glmath::vec2(1.0f, 0.0f), glmath::vec3(0.0f, 0.0f, 0.0f)},
-								 	 Vertex{glmath::vec3(-0.5f, -0.5f, 0.0f), glmath::vec2(0.0f, 0.0f), glmath::vec3(0.0f, 0.0f, 0.0f)},
-									 Vertex{glmath::vec3(-0.5f, 0.5f, 0.0f), glmath::vec2(0.0f, 1.0f), glmath::vec3(0.0f, 0.0f, 0.0f)} };
-
-	std::vector<uint32_t> indices = { 0, 3, 1,
-									 1, 3, 2 };
-									 
-	std::vector<glmath::vec3> colors = {glmath::vec3(0.0f, 0.0f, 0.0f),
-										glmath::vec3(0.0f, 0.0f, 0.0f),
-										glmath::vec3(0.0f, 0.0f, 0.0f),
-										glmath::vec3(0.0f, 0.0f, 0.0f),};
-
-	std::unique_ptr<Mesh> mesh(new Mesh());
-	mesh->init(vertices, indices, colors);
-	return mesh;
-}
-
 std::unique_ptr<Mesh> Mesh::createBox() {
 	std::vector<Vertex> vertices = {
-		Vertex { glm::vec3(-0.5f, -0.5f, -0.5f), glm::vec2(0.0f, 0.0f), glm::vec3( 0.0f, 0.0f, -1.0f) },
-		Vertex { glm::vec3( 0.5f, -0.5f, -0.5f), glm::vec2(1.0f, 0.0f), glm::vec3( 0.0f, 0.0f, -1.0f) },
-		Vertex { glm::vec3( 0.5f,  0.5f, -0.5f), glm::vec2(1.0f, 1.0f), glm::vec3( 0.0f, 0.0f, -1.0f) },
-		Vertex { glm::vec3(-0.5f,  0.5f, -0.5f), glm::vec2(0.0f, 1.0f), glm::vec3( 0.0f, 0.0f, -1.0f) },
+		Vertex { glmath::vec3(-0.5f, -0.5f, -0.5f), glmath::vec2(0.0f, 0.0f), glmath::vec3( 0.0f, 0.0f, -1.0f) },
+		Vertex { glmath::vec3( 0.5f, -0.5f, -0.5f), glmath::vec2(1.0f, 0.0f), glmath::vec3( 0.0f, 0.0f, -1.0f) },
+		Vertex { glmath::vec3( 0.5f,  0.5f, -0.5f), glmath::vec2(1.0f, 1.0f), glmath::vec3( 0.0f, 0.0f, -1.0f) },
+		Vertex { glmath::vec3(-0.5f,  0.5f, -0.5f), glmath::vec2(0.0f, 1.0f), glmath::vec3( 0.0f, 0.0f, -1.0f) },
 
-		Vertex { glm::vec3(-0.5f, -0.5f, 0.5f), glm::vec2(0.0f, 0.0f), glm::vec3( 0.0f, 0.0f, 1.0f)},
-		Vertex { glm::vec3( 0.5f, -0.5f, 0.5f), glm::vec2(1.0f, 0.0f), glm::vec3( 0.0f, 0.0f, 1.0f)},
-		Vertex { glm::vec3( 0.5f,  0.5f, 0.5f), glm::vec2(1.0f, 1.0f), glm::vec3( 0.0f, 0.0f, 1.0f)},
-		Vertex { glm::vec3(-0.5f,  0.5f, 0.5f), glm::vec2(0.0f, 1.0f), glm::vec3( 0.0f, 0.0f, 1.0f)},
+		Vertex { glmath::vec3(-0.5f, -0.5f, 0.5f), glmath::vec2(0.0f, 0.0f), glmath::vec3( 0.0f, 0.0f, 1.0f)},
+		Vertex { glmath::vec3( 0.5f, -0.5f, 0.5f), glmath::vec2(1.0f, 0.0f), glmath::vec3( 0.0f, 0.0f, 1.0f)},
+		Vertex { glmath::vec3( 0.5f,  0.5f, 0.5f), glmath::vec2(1.0f, 1.0f), glmath::vec3( 0.0f, 0.0f, 1.0f)},
+		Vertex { glmath::vec3(-0.5f,  0.5f, 0.5f), glmath::vec2(0.0f, 1.0f), glmath::vec3( 0.0f, 0.0f, 1.0f)},
 
-		Vertex { glm::vec3(-0.5f,  0.5f,  0.5f), glm::vec2(1.0f, 0.0f), glm::vec3(-1.0f, 0.0f, 0.0f)},
-		Vertex { glm::vec3(-0.5f,  0.5f, -0.5f), glm::vec2(1.0f, 1.0f), glm::vec3(-1.0f, 0.0f, 0.0f)},
-		Vertex { glm::vec3(-0.5f, -0.5f, -0.5f), glm::vec2(0.0f, 1.0f), glm::vec3(-1.0f, 0.0f, 0.0f)},
-		Vertex { glm::vec3(-0.5f, -0.5f,  0.5f), glm::vec2(0.0f, 0.0f), glm::vec3(-1.0f, 0.0f, 0.0f)},
+		Vertex { glmath::vec3(-0.5f,  0.5f,  0.5f), glmath::vec2(1.0f, 0.0f), glmath::vec3(-1.0f, 0.0f, 0.0f)},
+		Vertex { glmath::vec3(-0.5f,  0.5f, -0.5f), glmath::vec2(1.0f, 1.0f), glmath::vec3(-1.0f, 0.0f, 0.0f)},
+		Vertex { glmath::vec3(-0.5f, -0.5f, -0.5f), glmath::vec2(0.0f, 1.0f), glmath::vec3(-1.0f, 0.0f, 0.0f)},
+		Vertex { glmath::vec3(-0.5f, -0.5f,  0.5f), glmath::vec2(0.0f, 0.0f), glmath::vec3(-1.0f, 0.0f, 0.0f)},
 
-		Vertex { glm::vec3( 0.5f,  0.5f,  0.5f), glm::vec2(1.0f, 0.0f), glm::vec3( 1.0f, 0.0f, 0.0f)},
-		Vertex { glm::vec3( 0.5f,  0.5f, -0.5f), glm::vec2(1.0f, 1.0f), glm::vec3( 1.0f, 0.0f, 0.0f)},
-		Vertex { glm::vec3( 0.5f, -0.5f, -0.5f), glm::vec2(0.0f, 1.0f), glm::vec3( 1.0f, 0.0f, 0.0f)},
-		Vertex { glm::vec3( 0.5f, -0.5f,  0.5f), glm::vec2(0.0f, 0.0f), glm::vec3( 1.0f, 0.0f, 0.0f)},
+		Vertex { glmath::vec3( 0.5f,  0.5f,  0.5f), glmath::vec2(1.0f, 0.0f), glmath::vec3( 1.0f, 0.0f, 0.0f)},
+		Vertex { glmath::vec3( 0.5f,  0.5f, -0.5f), glmath::vec2(1.0f, 1.0f), glmath::vec3( 1.0f, 0.0f, 0.0f)},
+		Vertex { glmath::vec3( 0.5f, -0.5f, -0.5f), glmath::vec2(0.0f, 1.0f), glmath::vec3( 1.0f, 0.0f, 0.0f)},
+		Vertex { glmath::vec3( 0.5f, -0.5f,  0.5f), glmath::vec2(0.0f, 0.0f), glmath::vec3( 1.0f, 0.0f, 0.0f)},
 
-		Vertex { glm::vec3(-0.5f, -0.5f, -0.5f), glm::vec2(0.0f, 1.0f), glm::vec3( 0.0f, -1.0f, 0.0f)},
-		Vertex { glm::vec3( 0.5f, -0.5f, -0.5f), glm::vec2(1.0f, 1.0f), glm::vec3( 0.0f, -1.0f, 0.0f)},
-		Vertex { glm::vec3( 0.5f, -0.5f,  0.5f), glm::vec2(1.0f, 0.0f), glm::vec3( 0.0f, -1.0f, 0.0f)},
-		Vertex { glm::vec3(-0.5f, -0.5f,  0.5f), glm::vec2(0.0f, 0.0f), glm::vec3( 0.0f, -1.0f, 0.0f)},
+		Vertex { glmath::vec3(-0.5f, -0.5f, -0.5f), glmath::vec2(0.0f, 1.0f), glmath::vec3( 0.0f, -1.0f, 0.0f)},
+		Vertex { glmath::vec3( 0.5f, -0.5f, -0.5f), glmath::vec2(1.0f, 1.0f), glmath::vec3( 0.0f, -1.0f, 0.0f)},
+		Vertex { glmath::vec3( 0.5f, -0.5f,  0.5f), glmath::vec2(1.0f, 0.0f), glmath::vec3( 0.0f, -1.0f, 0.0f)},
+		Vertex { glmath::vec3(-0.5f, -0.5f,  0.5f), glmath::vec2(0.0f, 0.0f), glmath::vec3( 0.0f, -1.0f, 0.0f)},
 
-		Vertex { glm::vec3(-0.5f, 0.5f, -0.5f), glm::vec2(0.0f, 1.0f), glm::vec3( 0.0f, 1.0f, 0.0f)},
-		Vertex { glm::vec3( 0.5f, 0.5f, -0.5f), glm::vec2(1.0f, 1.0f), glm::vec3( 0.0f, 1.0f, 0.0f)},
-		Vertex { glm::vec3( 0.5f, 0.5f,	 0.5f), glm::vec2(1.0f, 0.0f), glm::vec3( 0.0f, 1.0f, 0.0f)},
-		Vertex { glm::vec3(-0.5f, 0.5f,	 0.5f), glm::vec2(0.0f, 0.0f), glm::vec3( 0.0f, 1.0f, 0.0f)},
+		Vertex { glmath::vec3(-0.5f, 0.5f, -0.5f), glmath::vec2(0.0f, 1.0f), glmath::vec3( 0.0f, 1.0f, 0.0f)},
+		Vertex { glmath::vec3( 0.5f, 0.5f, -0.5f), glmath::vec2(1.0f, 1.0f), glmath::vec3( 0.0f, 1.0f, 0.0f)},
+		Vertex { glmath::vec3( 0.5f, 0.5f,	 0.5f), glmath::vec2(1.0f, 0.0f), glmath::vec3( 0.0f, 1.0f, 0.0f)},
+		Vertex { glmath::vec3(-0.5f, 0.5f,	 0.5f), glmath::vec2(0.0f, 0.0f), glmath::vec3( 0.0f, 1.0f, 0.0f)},
 	};
 
 	std::vector<uint32_t> indices = {
@@ -60,59 +41,14 @@ std::unique_ptr<Mesh> Mesh::createBox() {
 		16, 17, 18, 18, 19, 16,
 		20, 22, 21, 22, 20, 23,
 	};
-									 
-	std::vector<glmath::vec3> colors = {
-		glmath::vec3(0.0f, 0.0f, 0.0f),
-		glmath::vec3(0.0f, 0.0f, 0.0f),
-		glmath::vec3(0.0f, 0.0f, 0.0f),
-		glmath::vec3(0.0f, 0.0f, 0.0f),
-
-		glmath::vec3(0.0f, 0.0f, 0.0f),
-		glmath::vec3(0.0f, 0.0f, 0.0f),
-		glmath::vec3(0.0f, 0.0f, 0.0f),
-		glmath::vec3(0.0f, 0.0f, 0.0f),
-
-		glmath::vec3(0.0f, 0.0f, 0.0f),
-		glmath::vec3(0.0f, 0.0f, 0.0f),
-		glmath::vec3(0.0f, 0.0f, 0.0f),
-		glmath::vec3(0.0f, 0.0f, 0.0f),
-
-		glmath::vec3(0.0f, 0.0f, 0.0f),
-		glmath::vec3(0.0f, 0.0f, 0.0f),
-		glmath::vec3(0.0f, 0.0f, 0.0f),
-		glmath::vec3(0.0f, 0.0f, 0.0f),
-
-		glmath::vec3(0.0f, 0.0f, 0.0f),
-		glmath::vec3(0.0f, 0.0f, 0.0f),
-		glmath::vec3(0.0f, 0.0f, 0.0f),
-		glmath::vec3(0.0f, 0.0f, 0.0f),
-
-		glmath::vec3(0.0f, 0.0f, 0.0f),
-		glmath::vec3(0.0f, 0.0f, 0.0f),
-		glmath::vec3(0.0f, 0.0f, 0.0f),
-		glmath::vec3(0.0f, 0.0f, 0.0f)
-	};
 
 	std::unique_ptr<Mesh> mesh(new Mesh());
-	mesh->init(vertices, indices, colors);
+	mesh->init(vertices, indices);
 	return mesh;
 }
 
-std::unique_ptr<Mesh> Mesh::createMesh(std::vector<Vertex>& vertices,
-										std::vector<uint32_t>& indices, 
-										std::vector<glmath::vec3>& colors,
-										glload::ObjInfo* objInfo) {
-	std::unique_ptr<Mesh> mesh(new Mesh());
-	mesh->init(vertices, indices, colors);
-	// mesh->initMaterial(objInfo->marterialInfo);
-	
-	return mesh;
-}
-
-
-void Mesh::init(std::vector<Vertex>& vertices, std::vector<uint32_t>& indices, std::vector<glmath::vec3>& colors) {
+void Mesh::init(std::vector<Vertex>& vertices, std::vector<uint32_t>& indices) {
 	GLsizei vertexSize = vertices.size();
-	GLsizei colorSize = colors.size();
 	m_elementSize = indices.size();
 
 	m_vertexArray = VertexArray::create();
@@ -123,10 +59,6 @@ void Mesh::init(std::vector<Vertex>& vertices, std::vector<uint32_t>& indices, s
 	m_vertexArray->setAttribute(1, 2, sizeof(Vertex), offsetof(Vertex, texCoord));
 	m_vertexArray->setAttribute(2, 3, sizeof(Vertex), offsetof(Vertex, normal));
 
-	m_vertexColorBuffer = Buffer::create(GL_ARRAY_BUFFER, sizeof(glmath::vec3) * colorSize,
-									colors.data(), GL_STATIC_DRAW);
-	m_vertexArray->setAttribute(3, 3, sizeof(glmath::vec3), 0);
-
 	m_elementBuffer = Buffer::create(GL_ELEMENT_ARRAY_BUFFER, sizeof(uint32_t) * m_elementSize,
 									indices.data(), GL_STATIC_DRAW);
 }
@@ -135,20 +67,3 @@ void Mesh::draw() {
 	m_vertexArray->bind();
 	glDrawElements(GL_TRIANGLES, m_elementSize, GL_UNSIGNED_INT, 0);
 }
-
-// void Material::setMaterial(glload::Material& materialInfo) {
-// 	for (int i = 0; i < 3; ++i) {
-// 		this->m_Ka[i] = materialInfo.Ka[i];
-// 		this->m_Kd[i] = materialInfo.Kd[i];
-// 		this->m_Ks[i] = materialInfo.Ks[i];
-// 	}
-
-// 	this->m_Ns = materialInfo.Ns;
-// 	this->m_Ni = materialInfo.Ni;
-// 	this->m_d = materialInfo.d;
-// 	this->m_illum = materialInfo.illum;
-// }
-
-// void Mesh::initMaterial(glload::Material& materialInfo) {
-// 	this->m_material.setMaterial(materialInfo);
-// }
