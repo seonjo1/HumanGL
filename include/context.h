681163a3a6a7830f684623b40cfd15e51162786b
seonjo1 <tjdgus9684@gmail.com>
2024-10-09 22:03:12 +0900

    Update context: 안 쓰는 부분 다 삭제 및 m_human 추가

--- include/context.h
+++ include/context.h
@@ -5,8 +5,6 @@
 # include "vertexArray.h"
 # include "buffer.h"
 # include "program.h"
-# include "image.h"
-# include "texture.h"
 # include "mesh.h"
 # include "model.h"
 
@@ -28,11 +26,8 @@ private:
 	glmath::vec3 m_cameraUp {0.0f, 1.0f, 0.0f};
 	glmath::vec3 m_cameraFront {0.0f, 0.0f, -1.0f};
 
-	std::unique_ptr<Mesh> m_plane;
-	std::unique_ptr<Model> m_model;
+	std::unique_ptr<Model> m_human;
 	std::unique_ptr<Program> m_program;
-	std::shared_ptr<Image> m_image;
-	std::unique_ptr<Texture> m_texture;
 };
 
 #endif
--- src/context.cpp
+++ src/context.cpp
@@ -5,62 +5,21 @@ void Context::Reshape(int width, int height) {
 	m_height = height;
 	glViewport(0, 0, m_width, m_height);
 }
-	
-void Context::ProcessInput(GLFWwindow *window)
-{	
-	const static float objectSpeed = 0.025f;
-	const static float rotateSpeed = 0.5f;
-	static float transSpeed = -0.01;
-	static bool keyTPressed = false;
-
-	float degree = 0.0f;
-	glmath::vec3 move(0.0f);
-	
-	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-		move = move + objectSpeed * m_cameraFront;
-	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-		move = move - objectSpeed * m_cameraFront;
-
-	glmath::vec3 cameraRight = glmath::normalize(glmath::cross(m_cameraFront, m_cameraUp));
-	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-		move = move + objectSpeed * cameraRight;
-	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-		move = move - objectSpeed * cameraRight;
-	
-	glmath::vec3 cameraUp = glmath::normalize(glmath::cross(cameraRight, m_cameraFront));
-	if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
-		move = move + objectSpeed * cameraUp;
-	if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS)
-		move = move - objectSpeed * cameraUp;
-
-	if (glfwGetKey(window, GLFW_KEY_R) == GLFW_PRESS)
-		degree = rotateSpeed;
-	
-	if (glfwGetKey(window, GLFW_KEY_T) == GLFW_PRESS) {
-        if (!keyTPressed) {
-            transSpeed = -transSpeed;
-            keyTPressed = true;
-        }
-    } else {
-        keyTPressed = false;
-    }
-	
-	m_model->updateModel(move, degree, transSpeed);
-}
 
 std::unique_ptr<Context> Context::create() {
 	std::unique_ptr<Context> context(new Context());
-	if (!context->init(objFile, bmpFile)) return nullptr;
+	if (!context->init()) return nullptr;
 	return context;
 }
 
 bool Context::init() {
 
-	m_program = Program::create("./shader/simple.vs", "./shader/simple.fs");
+	m_human = Model::createHuman();
+	// m_program = Program::create("./shader/simple.vs", "./shader/simple.fs");
 
-	if (m_program) {
-		return false;
-	}
+	// if (m_program) {
+	// 	return false;
+	// }
 
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_MULTISAMPLE);
@@ -73,12 +32,4 @@ void Context::Render() {
 	glClearColor(0.1f, 0.2f, 0.3f, 0.0f);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	glmath::mat4 projection = glmath::perspective(glmath::radians(45.0f), (float)m_width / (float)m_height , 0.01f, 60.0f);
-
-	m_program->useProgram();
-	m_program->setUniform("tex", 0);
-	m_program->setUniform("projection", projection);
-	m_texture->activeTexture(GL_TEXTURE0);
-	
-	m_model->draw(m_program.get(), m_cameraPos, m_cameraUp, m_cameraFront);
 }
