f15fc04aa46b662ad4c04372364ed54114d8b569
seonjo1 <tjdgus9684@gmail.com>
2024-10-14 15:22:53 +0900

    Update context.cpp: 배경 색 변경

--- src/context.cpp
+++ src/context.cpp
@@ -62,7 +62,7 @@ bool Context::init() {
 
 void Context::render() {
 
-	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	glClearColor(0.3f, 0.3f, 0.5f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 
